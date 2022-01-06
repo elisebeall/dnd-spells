a395cd97f2f503e8752654589333e69e5bda1140
August Reid <c.august.reid@gmail.com>
2022-01-05 19:49:39 -0700

    Map over spellDetails data to render Spell component as a link

--- src/components/SpellBox.js
+++ src/components/SpellBox.js
@@ -1,12 +1,26 @@
 import React from "react";
 import dataStub from "../testing/stub data";
 import { Link } from "react-router-dom";
+import Spell from "./Spell";
 
 const SpellBox = () => {
-    const spells = dataStub.spellDetails;
+    const allSpells = dataStub.spellDetails;
+
+    const spells = allSpells.map((spell) => {
+        return (
+            <Link to={`/spells/${spell.index}`} key={spell.index} > 
+            <Spell 
+                index={spell.index}
+                name={spell.name}
+                level={spell.level}
+                school={spell.school.name}
+            />
+            </Link>
+        )
+    })
 
     return (
-
+        {spells}
     )
 }
 
