a78bb91049f7aa834fba06f7030ada626af1f59a
Elise Beall <elisebeall@gmail.com>
2022-01-05 22:03:25 -0700

    Add basic responsive design to landing page

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,19 +1,24 @@
 import '../css/TopNav.css';
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { FaHatWizard } from 'react-icons/fa';
 
 const TopNav = () => {
   return (
     <header>
       <h1>Squizard's Spells</h1>
-      <div className="top-nav-btns">
-        <button className="home-btn">
-          Home
-        </button>
-        <button className="my-spellbook-btn">
-          <FaHatWizard className="icon-small" />
-          My Spellbook
-        </button>
+      <div className="top-nav-btns-box">
+        <Link to="/">
+          <button className="home-btn">
+            Home
+          </button>
+        </Link>
+        <Link to="/my-spellbook">
+          <button className="my-spellbook-btn">
+            <FaHatWizard className="icon-small" />
+            My Spellbook
+          </button>
+        </Link>
       </div>
     </header>
   )
