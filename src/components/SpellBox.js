618d842e1930cc1475ca9df3787af17fa7e12935
August Reid <c.august.reid@gmail.com>
2022-01-05 19:35:18 -0700

    Install and import react-router-dom

--- src/components/SpellBox.js
+++ src/components/SpellBox.js
@@ -1,9 +1,13 @@
 import React from "react";
 import dataStub from "../testing/stub data";
+import { Link } from "react-router-dom";
 
 const SpellBox = () => {
+    const spells = dataStub.spellDetails;
 
     return (
 
     )
 }
+
+export default SpellBox;
