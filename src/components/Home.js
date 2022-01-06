3088a39f86db5641b3cbd565881a651e37f1ce0a
Elise Beall <elisebeall@gmail.com>
2022-01-05 18:52:30 -0700

    Build out basic Home component structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,16 @@
-\
+import './css/Home.css';
+import React, { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import GiBlackBook from 'react-icons';
+import FaHatWizard from 'react-icons';
+
+const Home = () => {
+  return (
+    <>
+      <button><GiBlackBook />My Spellbook</button>
+      <button><FaHatWizard />My Spellbook</button>
+    </>
+  )
+}
+
+export const Home;
