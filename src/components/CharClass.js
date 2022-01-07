767b9c57596d7dda9086b0fdb8baa34a317950ca
Elise Beall <elisebeall@gmail.com>
2022-01-06 22:41:11 -0700

    Create custom hook for loading icons based on character class

--- src/components/CharClass.js
+++ src/components/CharClass.js
@@ -2,16 +2,18 @@ import '../css/CharClass.css';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import Bookmark from './Bookmark';
+import useIcon from '../hooks/useIcon';
 import charClassIcons from '../charClassIcons.js';
 
 const CharClass = ({ charClass }) => {
   const { index, name } = charClass;
-  
+  const { icon } = useIcon(index);
+
   return (
     <div className="char-class-card">
       <Bookmark />
       <Link to={index}>
-        <img src={charClassIcons.squizard} alt={`${name}`} />
+        <img src={icon} alt={`${name}`} />
         <h3>{name}</h3>
       </Link>
     </div>
