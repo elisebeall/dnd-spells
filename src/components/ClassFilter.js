84300aa822054b9450d999c7d0ba68672abe7f7e
August Reid <c.august.reid@gmail.com>
2022-01-10 21:50:32 -0700

    Format sidebar

--- src/components/ClassFilter.js
+++ src/components/ClassFilter.js
@@ -5,15 +5,15 @@ import "../css/ClassFilter.css"
 const ClassFilter = () => {
     return (
         <nav className="classNav">
-            <NavLink to={"/bard/spells"}>Bard</NavLink>
-            <NavLink to={"/cleric/spells"}>Cleric</NavLink>
-            <NavLink to={"/druid/spells"}>Druid</NavLink>
-            <NavLink to={"/paladin/spells"}>Paladin</NavLink>
-            <NavLink to={"/ranger/spells"}>Ranger</NavLink>
-            <NavLink to={"/sorcerer/spells"}>Sorcerer</NavLink>
-            <NavLink to={"/warlock/spells"}>Warlock</NavLink>
-            <NavLink to={"/wizard/spells"}>Wizard</NavLink>
-            <NavLink to={"/class"}>All Classes</NavLink>
+            <NavLink to={"/bard/spells"} className="classLink">Bard</NavLink>
+            <NavLink to={"/cleric/spells"} className="classLink">Cleric</NavLink>
+            <NavLink to={"/druid/spells"} className="classLink">Druid</NavLink>
+            <NavLink to={"/paladin/spells"} className="classLink">Paladin</NavLink>
+            <NavLink to={"/ranger/spells"} className="classLink">Ranger</NavLink>
+            <NavLink to={"/sorcerer/spells"} className="classLink">Sorcerer</NavLink>
+            <NavLink to={"/warlock/spells"} className="classLink">Warlock</NavLink>
+            <NavLink to={"/wizard/spells"} className="classLink">Wizard</NavLink>
+            <NavLink to={"/class"} className="classLink">All Classes</NavLink>
         </nav>
     )
 }
