e46823610dd1e5af591058f2f3ece3c92133960c
August Reid <c.august.reid@gmail.com>
2022-01-10 20:57:30 -0700

    Add NavLinks for 8 magical classes

--- src/components/ClassFilter.js
+++ src/components/ClassFilter.js
@@ -2,6 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../css/ClassFilter.css"
 
-
+const ClassFilter = () => {
+    return (
+        <nav>
+            <NavLink to={"/bard/spells"}>Bard</NavLink>
+            <NavLink to={"/cleric/spells"}>Cleric</NavLink>
+            <NavLink to={"/druid/spells"}>Druid</NavLink>
+            <NavLink to={"/paladin/spells"}>Paladin</NavLink>
+            <NavLink to={"/ranger/spells"}>Ranger</NavLink>
+            <NavLink to={"/sorcerer/spells"}>Sorcerer</NavLink>
+            <NavLink to={"/warlock/spells"}>Warlock</NavLink>
+            <NavLink to={"/wizard/spells"}>Wizard</NavLink>
+        </nav>
+    )
+}
 
 export default ClassFilter;
