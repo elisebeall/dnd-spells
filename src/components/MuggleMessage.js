a5e7370087f742c588ed72bee4ee79c2e12697de
August Reid <c.august.reid@gmail.com>
2022-01-12 16:50:21 -0700

    Adjust font and spacing for MuggleMessage

--- src/components/MuggleMessage.js
+++ src/components/MuggleMessage.js
@@ -5,7 +5,7 @@ const MuggleMessage = ({ charClass }) => {
   return (
    <section className='message'>
     <h2>{`Squizzard is sorry, ${charClass}s don't get to do any magic!`}</h2>
-    <h3>Maybe go find some rocks to throw at the goblins...</h3>
+    <h3 className='plain-font'>Maybe go find some rocks to throw at the goblins...</h3>
    </section>
   )
 }
