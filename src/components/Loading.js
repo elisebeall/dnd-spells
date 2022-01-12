297954cc1e2d7451144cd95531e9492b9eaba186
Elise Beall <elisebeall@gmail.com>
2022-01-12 14:12:02 -0700

    Add Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+import loading from '../assets/squiz.gif';
+
+const Loading = () => {
+  return (
+    <img src={loading} alt="animation of a squirrel eating a nut" />
+  )
+}
+
+export default Loading;
