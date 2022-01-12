dcc4f8761053036aad819477bd94974306b9873f
August Reid <c.august.reid@gmail.com>
2022-01-11 17:56:53 -0700

    Add Bookmark to SpellDetails

--- src/components/SpellDetails.js
+++ src/components/SpellDetails.js
@@ -2,12 +2,18 @@ import React, { Fragment } from 'react';
 import { useParams } from 'react-router-dom'
 import ClassFilter from './ClassFilter';
 import '../css/SpellDetails.css';
+import Bookmark from './Bookmark';
 import useFetch from '../hooks/useFetch';
 
 const SpellDetails = ({}) => {
   const spellIndex = useParams().index;
   const { data: details, isLoading, error } = useFetch(`https://www.dnd5eapi.co/api/spells/${spellIndex}`)
-    
+  let spell;
+
+  if (!isLoading) {
+   spell = {"index": details.index, "name": details.name, "url": `https://www.dnd5eapi.co/api/spells/${spellIndex}`}
+  }
+
   return(
     <div className="detailsBox">
       <ClassFilter />
@@ -17,6 +23,7 @@ const SpellDetails = ({}) => {
             <>
               {     
                 <div className='individual-spell'>
+                  <Bookmark spell={spell}/>
                   <h3>{details.name}</h3>
                   <table className='details'>
                     <tr>
